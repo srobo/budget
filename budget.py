8e948ca700ac3bccd5a255deb97af32cf6bbff92
Robert Spanton <rspanton@zepler.net>
2011-08-22 03:01:10 +0100

    Don't add every item as a child of a BudgetTree of itself
    Bluuuurg.  Additional fixes...

--- budget.py
+++ budget.py
@@ -95,7 +95,7 @@ def load_budget(root):
             name = fullp[len(root)+1:-5]
 
             r = tree
-            for d in name.split("/"):
+            for d in name.split("/")[:-1]:
                 if d not in r.children:
                     r.add_child( BudgetTree(d) )
 
